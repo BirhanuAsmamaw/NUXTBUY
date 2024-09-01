15cecc7e2586cfc4b89f9bebb38a59791c719656
BirhanuAsmamaw <birhanuasmamaw24@gmail.com>
2024-09-01 02:44:33 -0900

    my first commit

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h2>About</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Incidunt quasi at perspiciatis quaerat. Cum quae necessitatibus commodi corrupti omnis odit?</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Incidunt quasi at perspiciatis quaerat. Cum quae necessitatibus commodi corrupti omnis odit?</p>
+      </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
