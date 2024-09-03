c918c509a24f5887009afe6d1b245fe139909c62
BirhanuAsmamaw <birhanuasmamaw24@gmail.com>
2024-09-02 23:44:32 -0900

    adding product details param

---
+++ pages/products/[id].vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p> Product details for {{ id }}</p>
+    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Ratione eligendi ipsum fuga quaerat veritatis! Magni molestiae accusantium nostrum eius 
+        ipsam in maiores ab deleniti enim quisquam fugiat facere optio, ipsa voluptatem! Natus amet nulla doloribus magnam odio totam ipsam odit.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute.params
+</script>
+
+<style  scoped>
+
+</style>
