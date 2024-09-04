60d2c92f85350c1c61fc1452a56428a383e27fcd
BirhanuAsmamaw <birhanuasmamaw24@gmail.com>
2024-09-04 07:10:52 -0900

    reusable componenes

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -3,8 +3,8 @@
 <template>
     <div>
        <div class="grid grid-cols-4 gap-5">
-         <div v-for="product in products">
-              <NuxtLink :to="`/products/${product.id}`"> {{ product.title }}</NuxtLink>
+         <div v-for="p in products">
+            <ProductCard :product ="p" />
          </div>
        </div>
     </div>
