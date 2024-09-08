0f311fc1748bb86f1c02f707fa738b26b874afb2
BirhanuAsmamaw <birhanuasmamaw24@gmail.com>
2024-09-07 23:35:15 -0900

    adding products

--- layouts/products.vue
+++ layouts/products.vue
@@ -12,11 +12,13 @@
        </div>
 
        <footer class="container mx-auto p-4 flex justify-between border-t-2">
+
         <ul class="flex gap-4">
             <li><NuxtLink to="/">Home</NuxtLink> </li>
             <li><NuxtLink to="/About">About</NuxtLink> </li>
             <li><NuxtLink to="/products">Products</NuxtLink> </li>
           </ul>
+          
        </footer>
 
       </div>
